3a194f0507964a12bbd2fdba05579e98169c4d01
Mary Wells <m.a.wells@outlook.com>
2021-09-22 16:28:39 +0100

    initialise app

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import tailwind from 'tailwind-rn';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={tailwind(style.body)}>
+      <Text style={tailwind(style.header)}>Swim Good App!</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const style = {
+  body: 'bg-yellow-200 justify-center items-center flex-1',
+  header: 'text-blue-900 font-bold',
+};
