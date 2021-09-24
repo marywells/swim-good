0e67c0a6faed5cb35a2aeada7517fff0678204e1
Mary Wells <m.a.wells@outlook.com>
2021-09-24 11:53:37 +0100

    refactor: optimise search box

--- components/search-box.js
+++ components/search-box.js
@@ -4,74 +4,58 @@ import { View, Text, TouchableOpacity, FlatList } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import { BEACHES } from './testdata';
 
-// class ItemList extends React.PureComponent {
-//   render() {
-//     const { item } = this.props;
-//     return (
-//       <TouchableOpacity
-//         onPress={() => {
-//           selectedBeach(item);
-//         }}
-//       >
-//         <Text style={tailwind(style.beachName)}>{item.label}</Text>
-//       </TouchableOpacity>
-//     );
-//   }
-// }
-
 export function SearchBox({ navigation, updateBeach }) {
-  const [query, setQuery] = useState('');
-  let beaches = BEACHES.slice();
+  const [state, setState] = useState({
+    data: BEACHES,
+    searchValue: '',
+  });
 
-  function filterBeachNames(beach) {
-    let search = query.toLowerCase();
-    if (beach.label.toLowerCase().startsWith(search)) {
-      return `${beach.label}`;
-    } else {
-      beaches.splice(beaches.indexOf(beach), 1);
-    }
-  }
-  //update query state with user input
-  function updateQuery(input) {
-    setQuery(input);
-    beaches = BEACHES.slice();
-  }
-
-  //when beach is selected, call ApiService, update state, navigate to beach
-  function selectedBeach(item) {
-    updateBeach(item);
-    navigation.navigate('Beach');
+  function searchFunction(text) {
+    const updatedData = BEACHES.filter((item) => {
+      const target = `${item.label.toLowerCase()})`;
+      const input = text.toLowerCase();
+      return target.indexOf(input) > -1;
+    });
+    setState({ data: updatedData, searchValue: text });
   }
 
   function renderItem({ item }) {
     return (
       <TouchableOpacity
         onPress={() => {
-          selectedBeach(item);
+          updateBeach(item);
+          navigation.navigate('Beach');
         }}
       >
-        <Text style={tailwind(style.beachName)}>{filterBeachNames(item)}</Text>
+        <Text style={tailwind(style.item)}>{item.label}</Text>
       </TouchableOpacity>
     );
-    //return <ItemList item={item} />;
   }
 
   return (
     <View>
-      <SearchBar
-        style={tailwind(style.searchInput)}
-        containerStyle={tailwind(style.searchContainer)}
-        lightTheme={true}
-        onChangeText={updateQuery}
-        value={query}
-        placeholder='Find a beach...'
-      />
+      <View style={tailwind(style.searchBarContainer)}>
+        <SearchBar
+          placeholder='Find a beach'
+          lightTheme
+          round
+          value={state.searchValue}
+          containerStyle={{
+            backgroundColor: 'transparent',
+            border: 'none',
+          }}
+          inputStyle={{ backgroundColor: 'transparent', color: 'white' }}
+          inputContainerStyle={{ backgroundColor: 'transparent' }}
+          placeholderTextColor={'#ffffff'}
+          onChangeText={(text) => searchFunction(text)}
+        />
+      </View>
 
-      {query.length > 0 && (
+      {state.searchValue.length > 2 && (
         <FlatList
-          data={beaches}
-          keyExtractor={(i) => i.EUBWID}
+          data={state.data}
           renderItem={renderItem}
+          keyExtractor={(item) => item.EUBWID}
         />
       )}
     </View>
@@ -79,7 +63,6 @@ export function SearchBox({ navigation, updateBeach }) {
 }
 
 const style = {
-  searchContainer: 'bg-white',
-  searchInput: 'bg-white',
-  beachName: 'text-xl p-3',
+  searchBarContainer: 'bg-white bg-opacity-20 mt-10 ',
+  item: 'text-lg p-3 text-white',
 };
