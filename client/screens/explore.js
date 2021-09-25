e8d54da31631926651afea02ff2c0da7450ff1a6
Mary Wells <m.a.wells@outlook.com>
2021-09-25 21:44:41 +0100

    feat: explore icons - display, onclick & navigate

--- client/screens/explore.js
+++ client/screens/explore.js
@@ -1,10 +1,10 @@
 import tailwind from 'tailwind-rn';
 import React from 'react';
 import { View, Text } from 'react-native';
+import { BEACHES } from '../data/beach-data';
+import MapView, { Marker, Callout } from 'react-native-maps';
 
-import MapView from 'react-native-maps';
-
-export function Explore({ navigation }) {
+export function Explore({ navigation, updateBeach }) {
   return (
     <View style={tailwind(style.body)}>
       <MapView
@@ -12,15 +12,32 @@ export function Explore({ navigation }) {
         initialRegion={{
           latitude: 51.509865,
           longitude: -0.118092,
-          latitudeDelta: 4,
-          longitudeDelta: 4,
+          latitudeDelta: 3,
+          longitudeDelta: 3,
         }}
       >
-        <MapView.Marker
-          coordinate={{ latitude: 51.494888, longitude: -0.12961 }}
-          title={'mary'}
-          description={'mary is here'}
-        />
+        {BEACHES.map((beach) => (
+          <Marker
+            key={beach.EUBWID}
+            coordinate={{ latitude: beach.lat, longitude: beach.long }}
+            pinColor={beach.swimBan === true ? 'red' : 'blue'}
+          >
+            <Callout
+              onPress={() => {
+                updateBeach(beach);
+                navigation.navigate('Beach');
+              }}
+            >
+              <Text>
+                {beach.label}
+                {'\n'}
+                {beach.swimBan === true
+                  ? '🚫 Advise against bathing'
+                  : '✔️ Safe to swim'}
+              </Text>
+            </Callout>
+          </Marker>
+        ))}
       </MapView>
     </View>
   );
