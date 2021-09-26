af957352d93a3ee2cfdb7a049ea2fb30fc696009
Mary Wells <m.a.wells@outlook.com>
2021-09-26 10:07:10 +0100

    style: explore page

--- client/screens/explore.js
+++ client/screens/explore.js
@@ -2,38 +2,46 @@ import tailwind from 'tailwind-rn';
 import React from 'react';
 import { View, Text } from 'react-native';
 import { BEACHES } from '../data/beach-data';
-import MapView, { Marker, Callout } from 'react-native-maps';
+import MapView from 'react-native-map-clustering';
+import { Marker, Callout } from 'react-native-maps';
 
 export function Explore({ navigation, updateBeach }) {
+  const INITIAL_REGION = {
+    latitude: 51.38770455,
+    longitude: 1.37575226,
+    latitudeDelta: 0.5,
+    longitudeDelta: 0.5,
+  };
+
   return (
     <View style={tailwind(style.body)}>
       <MapView
-        style={{ flex: 1, width: 600, height: 300 }}
-        initialRegion={{
-          latitude: 51.509865,
-          longitude: -0.118092,
-          latitudeDelta: 3,
-          longitudeDelta: 3,
-        }}
+        style={{ flex: 1 }}
+        minPoints={3}
+        clusterColor={'#29b6f6'}
+        initialRegion={INITIAL_REGION}
       >
         {BEACHES.map((beach) => (
           <Marker
             key={beach.EUBWID}
             coordinate={{ latitude: beach.lat, longitude: beach.long }}
-            pinColor={beach.swimBan === true ? 'red' : 'blue'}
+            pinColor={beach.swimBan === true ? '#ef5350' : '#29b6f6'}
           >
             <Callout
+              style={tailwind(style.callout)}
               onPress={() => {
                 updateBeach(beach);
                 navigation.navigate('Beach');
               }}
             >
-              <Text>
+              <Text style={tailwind(style.calloutBeach)}>
                 {beach.label}
                 {'\n'}
-                {beach.swimBan === true
-                  ? '🚫 Advise against bathing'
-                  : '✔️ Safe to swim'}
+                <Text style={tailwind(style.calloutSwim)}>
+                  {beach.swimBan === true
+                    ? '🚫 Advise against bathing'
+                    : '✔️ Safe to swim'}
+                </Text>
               </Text>
             </Callout>
           </Marker>
@@ -45,4 +53,6 @@ export function Explore({ navigation, updateBeach }) {
 
 const style = {
   body: 'bg-blue-400 h-full',
+  calloutBeach: 'font-bold text-center',
+  calloutSwim: 'font-thin text-center',
 };
